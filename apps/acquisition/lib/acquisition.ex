57a502e765a65d635f48ec6d6cbfae1e87dfea75
David Robertsson <david@aldb.se>
2021-03-21 11:32:52 +0100

    Initial commit Acquisition task
    The acquisition module should be able to fire up bots making the deals when the
    Trades.Leader finds the market favorable.
    
    The acquisition bot, will start looking at the orderbook and try to find as
    optimal trade as possible.
    
    When a bot is active, trades depends on strategy selected.
    At moment Im thinking about
    Mean reversion.
    I also have some thoughts trying to use the momentum to do a more naive strategy
    to buy/sell at multiple points to minimize the risk of placing a bad buy.
    
    Will probably use the name moment and mean.

---
+++ apps/acquisition/lib/acquisition.ex
@@ -0,0 +1,18 @@
+defmodule Acquisition do
+  @moduledoc """
+  Documentation for `Acquisition`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Acquisition.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/acquisition/test/acquisition_test.exs
@@ -0,0 +1,8 @@
+defmodule AcquisitionTest do
+  use ExUnit.Case
+  doctest Acquisition
+
+  test "greets the world" do
+    assert Acquisition.hello() == :world
+  end
+end
